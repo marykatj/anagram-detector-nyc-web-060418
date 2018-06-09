af13d2dac231025573d96b63091a51974e644694
Mary-Kate James <marykatj@gmail.com>
2018-06-09 20:11:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   def match(anagram_array)
     empty_array = []
     anagram_array.select do |anagram|
-      anagram == @word
+      anagram.split("".sort)
     end
     else
       empty_array
